1477c53103d31e32219bede149ff3734c0e8fa16
Zach Thomas <zwthomas13@gmail.com>
2020-03-25 21:53:40 -0500

    Got the bars to move with bubble sort. had to change the visualizer to a class component it would rerender when doing the sorting

--- src/components/bar/Bar.js
+++ src/components/bar/Bar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Bar.css";
 
 const Bar = ({percent}) => {
-    return <div className="bar" style={{ height: `${percent}%`, width: `${500/3}px`}} />;
+    return <div className="bar" style={{ height: `${percent}%`, width: `${500}px`}} />;
 }
 
 export default Bar;
--- src/components/visualizer/Visualizer.js
+++ src/components/visualizer/Visualizer.js
@@ -1,16 +1,54 @@
 import "./Visualizer.css";
-import React from "react";
+import React, { useState } from "react";
 
 import Bar from "./../bar/Bar";
+import { useEffect } from "react";
 
-function Visualizer(props) {
-  return (
-    <div className="container">
-      <Bar percent={30} />
-      <Bar percent={40} />
-      <Bar percent={50} />
-    </div>
-  );
+class Visualizer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { percents: [ 60, 50,90, 30] };
+
+    this.bubbleSort = this.bubbleSort.bind(this);
+  }
+  sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  async bubbleSort() {
+    let newArray = this.state.percents;
+
+    for (let i = 0; i < newArray.length; i++) {
+      for (let j = 0; j < newArray.length; j++) {
+        if (newArray[j] > newArray[j+1]) {
+          let temp = newArray[j];
+          newArray[j] = newArray[j + 1];
+          newArray[j + 1] = temp;
+          this.setState(newArray);
+          await this.sleep(500)
+        }
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className="temp">
+        <div className="container">
+          {this.state.percents.map(function(percent) {
+            return <Bar percent={percent} />;
+          })}
+        </div>
+        <button
+          onClick={() => {
+            this.bubbleSort();
+          }}
+        >
+          Sort
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Visualizer;
